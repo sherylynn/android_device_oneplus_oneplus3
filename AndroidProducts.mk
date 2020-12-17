1079473c389413a54246b021e62aa90b0d7b63eb
sherylynn <352281674@qq.com>
2020-12-17 17:58:20 +0800

    switch maruos

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_oneplus3.mk
+    $(LOCAL_DIR)/maru_oneplus3.mk
+    #$(LOCAL_DIR)/lineage_oneplus3.mk \
+
+COMMON_LUNCH_CHOICES := \
+	maru_oneplus3-userdebug \
+	maru_oneplus3-eng
